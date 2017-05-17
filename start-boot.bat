cd7cf889ba2f2b78b5195108fdeb9deaca8d2a1c
Chandan Gupta <P7110858@ntil.com>
2017-05-17 10:45:11 +0530

    changes in start-boot.bat

--- start-boot.bat
+++ start-boot.bat
@@ -1,2 +1,2 @@
-::java -jar target/nco-rest-api-0.1.jar --server.port=8085
-java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=n -jar target/nco-rest-api-0.1.jar --server.port=8085
+java -jar target/nco-rest-api-0.1.jar --server.port=8085
+::java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=n -jar target/nco-rest-api-0.1.jar --server.port=8085
